38d3825359b29632c6bf55e13af5503f4d41598a
Osamu TONOMORI <osamingo@gmail.com>
2018-05-26 14:28:11 +0900

    Add example

---
+++ _example/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+
+	"github.com/osamingo/gosh"
+)
+
+func main() {
+
+	const path = "/healthz"
+
+	mux := http.NewServeMux()
+	mux.Handle(path, gosh.NewStatsHandler())
+
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	resp, err := http.Get(srv.URL + path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalln("unexpect status code")
+	}
+
+	io.Copy(os.Stdout, resp.Body)
+}
